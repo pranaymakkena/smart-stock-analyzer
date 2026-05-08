9cf4e084ea7d4348cd477648b06a0133fb81dc53
Pranay Makkena <pranaymakkena@gmail.com>
2026-05-08 17:59:02 +0530

    Fix

--- backend/src/main/java/com/stockanalyzer/SmartStockAnalyzerApplication.java
+++ backend/src/main/java/com/stockanalyzer/SmartStockAnalyzerApplication.java
@@ -2,10 +2,11 @@ package com.stockanalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { MailSenderAutoConfiguration.class })
 @EnableCaching
 @EnableScheduling
 public class SmartStockAnalyzerApplication {
